76b963a7d36c804fbd4ab8e29f6d2b40ab95217b
ineg22 <ineg22@gmail.com>
2019-10-04 18:16:13 +0300

    feat: task ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.predict = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.predict = Math.round((this.max + this.min) / 2);
+        return this.predict;
     }
 
     lower() {
-
+        this.max = this.predict;
     }
 
     greater() {
-
+        this.min = this.predict;
     }
 }
 
